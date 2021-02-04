480743552fb0fe987def3e90979cc91d5afd7e18
sauhardo <sauhardo2007@gmail.com>
2021-02-04 21:17:09 +0530

    InitalCommit

---
+++ Screens/WelcomeScreen.js
@@ -0,0 +1,378 @@
+import * as React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image, Modal, ScrollView, KeyboardAvoidingView } from 'react-native'
+import firebase from 'firebase'
+import db from "../config"
+
+
+export default class WelcomeScreen extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+
+      EmailID: '',
+      Password: '',
+      FirstName: '',
+      LastName: '',
+      Address: '',
+      contactNo: '',
+      ConfirmPassword: '',
+      isModalVisible: false
+
+    }
+  }
+
+
+  ShowModal = () => {
+
+    return (
+
+      <Modal animationType='fade' transparent={true} visible={this.state.isModalVisible}>
+
+        <View style={styles.ModelConatiner}>
+
+          <ScrollView style={{ width: '100%' }}>
+
+            <KeyboardAvoidingView style={styles.FoarmView}>
+
+              <Text style={styles.FoarmTitle}>Registration</Text>
+
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='FirstName'
+                maxLength={10}
+
+                onChangeText={(text) => {
+
+                  this.setState({ FirstName: text })
+
+                }}
+              />
+
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='LastName'
+                maxLength={10}
+
+                onChangeText={(text) => {
+
+                  this.setState({ LastName: text })
+
+                }}
+              />
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='ContactNumber'
+                maxLength={10}
+                keyboardType='numeric'
+                onChangeText={(text) => {
+
+                  this.setState({ contactNo: text })
+
+                }}
+              />
+
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='Address'
+                multiline={true}
+                onChangeText={(text) => {
+
+                  this.setState({ Address: text })
+
+                }}
+              />
+
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='Email'
+                keyboardType='email-address'
+                onChangeText={(text) => {
+
+                  this.setState({ EmailID: text })
+
+                }}
+              />
+
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='Password'
+                secureTextEntry={true}
+                onChangeText={(text) => {
+
+                  this.setState({ Password: text })
+
+                }}
+              />
+
+              <TextInput
+
+                style={styles.FoarmTextInput}
+                placeholder='ConfirmPassword'
+                secureTextEntry={true}
+                onChangeText={(text) => {
+
+                  this.setState({ ConfirmPassword: text })
+
+                }}
+              />
+              <View>
+
+                <TouchableOpacity style={styles.RegisterButton}>
+
+                  <Text style={styles.RegisterButtonText}>Register</Text>
+
+                </TouchableOpacity>
+
+              </View>
+
+              <View>
+
+                <TouchableOpacity style={styles.RegisterButton}>
+
+                  <Text style={styles.RegisterButtonText}>Cancel</Text>
+
+                </TouchableOpacity>
+
+              </View>
+
+            </KeyboardAvoidingView>
+
+          </ScrollView>
+
+        </View>
+
+      </Modal>
+
+
+    )
+
+  }
+  UserSignUp = (Email, Password) => {
+
+    firebase.auth().createUserWithEmailAndPassword(Email, Password)
+      .then((response) => {
+
+        return alert("User Added Sucesfully")
+
+      })
+      .catch(function (error) {
+
+        var errorMessage = error.message;
+        return alert(errorMessage);
+      })
+  }
+
+  userLogin = (Email, Password) => {
+
+    firebase.auth().signInWithEmailAndPassword(Email, Password)
+      .then((response) => {
+
+        return alert("login was sucessful")
+
+      })
+      .catch(function (error) {
+
+        var errorMessage = error.message;
+        return alert(errorMessage);
+      })
+  }
+
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+      
+        <View style={styles.innerContainer}>
+          {this.ShowModal()}
+          <Text style={styles.Title}>BookSanta</Text>
+
+        </View>
+
+        <View>
+          <Image
+
+            source={require("../assets/rocksanta.gif")}
+            style={{ width: 200, height: 100, alignSelf: 'center' }}
+
+          />
+          <TextInput
+
+            style={styles.loginBox}
+            placeholder="abc@example.com"
+            keyboardType='email-address'
+            onChangeText={(text) => {
+              this.setState({ EmailID: text })
+            }}
+          />
+
+
+          <TextInput
+
+            style={styles.loginBox}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(text) => {
+              this.setState({ Password: text })
+            }}
+          />
+
+          <TouchableOpacity style={[styles.button, { marginTop: 20, marginBottom: 20 }]} onPress={() => {
+
+            //this.UserSignUp(this.state.EmailID, this.state.Password)
+            this.setState({isModalVisible: true})
+
+          }}>
+
+            <Text style={styles.buttonText}>SignUp</Text>
+
+
+          </TouchableOpacity>
+
+
+          <TouchableOpacity style={styles.button} onPress={() => {
+
+            this.userLogin(this.state.EmailID, this.state.Password)
+
+
+          }}>
+
+            <Text style={styles.buttonText}>Login</Text>
+
+
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    alignSelf: "center",
+    alignItems: 'center',
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: 'lightblue',
+
+  },
+  Title: {
+
+    fontSize: 65,
+    paddingBottom: 30,
+    fontWeight: '300',
+    color: 'white'
+
+  },
+
+  loginBox: {
+
+    width: 300,
+    height: 40,
+    borderBottomWidth: 1.5,
+    borderColor: 'grey',
+    fontSize: 20,
+    margin: 10,
+    paddingLeft: 10
+
+  },
+
+  button: {
+
+    width: 300,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+    borderRadius: 3
+  },
+
+  buttonText: {
+
+    fontSize: 20,
+    fontWeight: '200',
+    color: 'white'
+
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 200
+  },
+
+  FoarmView: {
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  FoarmTitle: {
+
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize: 30,
+    color: 'orange',
+    margin: 50
+  },
+
+  FoarmTextInput: {
+
+    width: '75%',
+    height: 35,
+    alignSelf: 'center',
+    borderColor: 'grey',
+    borderRadius: 10,
+    borderWidth: 1,
+    marginTop: 20,
+    padding: 10
+  },
+
+  ModelConatiner: {
+
+    flex: 1,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginRight: 30,
+    marginLeft: 30,
+    marginTop: 80,
+    marginBottom: 80
+
+  },
+
+  RegisterButton: {
+
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 30
+
+  },
+
+  RegisterButtonText: {
+
+    color: 'black',
+    fontSize: 15,
+    fontWeight: 'bold'
+
+
+
+  }
+})
---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+
+const firebaseConfig = {
+ apiKey: "AIzaSyBTp_eJF67c21UVhIAQdAiJ_9TQi3MDvM4",
+ authDomain: "booksanta-1-53257.firebaseapp.com",
+ projectId: "booksanta-1-53257",
+ storageBucket: "booksanta-1-53257.appspot.com",
+ messagingSenderId: "663297695070",
+ appId: "1:663297695070:web:70d17c1cf6df7bbc6943cf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
